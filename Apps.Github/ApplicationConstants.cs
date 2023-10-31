133e8dd8fa5defbd2b00e1722b82d6b58985b67e
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-31 15:43:40 +0200

    secrets placeholders changed

--- Apps.Github/ApplicationConstants.cs
+++ Apps.Github/ApplicationConstants.cs
@@ -8,12 +8,12 @@ namespace Apps.Github
 {
     public class ApplicationConstants
     {
-        public const string ClientId = "#{GITHUB_CLIENT_ID}#";
+        public const string ClientId = "#{APP_GITHUB_CLIENT_ID}#";
 
-        public const string ClientSecret = "#{GITHUB_SECRET}#";
+        public const string ClientSecret = "#{APP_GITHUB_SECRET}#";
 
-        public const string Scope = "#{GITHUB_SCOPE}#";
+        public const string Scope = "#{APP_GITHUB_SCOPE}#";
 
-        public const string BlackbirdToken = "#{GITHUB_BLACKBIRD_TOKEN}#"; // bridge validates this token
+        public const string BlackbirdToken = "#{APP_GITHUB_BLACKBIRD_TOKEN}#"; // bridge validates this token
     }
 }
