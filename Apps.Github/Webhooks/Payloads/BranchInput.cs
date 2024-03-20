dab16c3e00cd1936ee9b0e3711aef38edfd356cc
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-03-20 16:13:41 +0200

    Ux improvements, bugs fixed

--- Apps.Github/Webhooks/Payloads/BranchInput.cs
+++ Apps.Github/Webhooks/Payloads/BranchInput.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Apps.GitHub.Webhooks.Payloads
 {
     public class BranchInput
     {
+        [Display("Branch name")]
         public string? BranchName { get; set; }
     }
 }
