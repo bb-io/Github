dbf5a76a9ba141c7bae140abdbbfff0a3d8cb3a4
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-08-22 14:34:12 +0300

    Exceed batch size limit test

--- Apps.Github/Models/Commit/Responses/PullRequestFilesResponse.cs
+++ Apps.Github/Models/Commit/Responses/PullRequestFilesResponse.cs
@@ -6,7 +6,7 @@ namespace Apps.GitHub.Models.Commit.Responses
 {
     public class PullRequestFilesResponse(List<PullRequestFileDto> files)
     {
-        public IEnumerable<PullRequestFileDto> Files { get; set; } = files;
+        //public IEnumerable<PullRequestFileDto> Files { get; set; } = files;
 
         [Display("Total count")]
         public int TotalCount { get; set; } = files.Count;
