b5be34b5dfc148a8f2531895c7f604c3e06681bc
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-25 18:34:11 +0200

    update FolderInput.FolderPath display name, update docs

--- Apps.Github/Webhooks/Payloads/FolderInput.cs
+++ Apps.Github/Webhooks/Payloads/FolderInput.cs
@@ -6,7 +6,7 @@ namespace Apps.Github.Webhooks.Payloads;
 
 public class FolderInput
 {
-    [Display("Folder path", Description = "Use the forward slash '/' to represent directory separator. Use '*' to represent wildcards in file and directory names. Use '**' to represent arbitrary directory depth.")]
+    [Display("Folder pattern", Description = "Use the forward slash '/' to represent directory separator. Use '*' to represent wildcards in file and directory names. Use '**' to represent arbitrary directory depth.")]
     public string? FolderPath { get; set; }
 
     [FileDataSource(typeof(FolderPathDataHandler))]
