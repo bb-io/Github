a394b00d41fe8e9f4b9067f63497eb548d096a44
Artem Riabushenko <ariabushenko@blackbird.io>
2026-06-16 13:49:32 +0300

    Adjustments to output model in 'Trigger workflow' action

--- Apps.Github/Models/Workflow/WorkflowDispatchResponseDto.cs
+++ Apps.Github/Models/Workflow/WorkflowDispatchResponseDto.cs
@@ -5,7 +5,7 @@
         public string Repository { get; set; } = default!;
         public string Workflow { get; set; } = default!;
         public string Ref { get; set; } = default!;
-        public object? Inputs { get; set; }
+        public string? InputsJson { get; set; }
         public string Status { get; set; } = "requested";
     }
 }
